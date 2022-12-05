56074c3cb46001d23b40e4c2e69ba735c5be87e5
Mariana Galvão <81964220+Mariana-gs@users.noreply.github.com>
2022-12-05 11:10:57 -0300

    >:(

--- AlgoritmoDeTomasulo/src/InterfaceGrafica/Instrucoes.java
+++ AlgoritmoDeTomasulo/src/InterfaceGrafica/Instrucoes.java
@@ -2,30 +2,57 @@ package InterfaceGrafica;
 
 import java.util.LinkedList;
 
+
 class Instrucoes {
     
     public int numeroInstrucoes;
     public int numeroTiposInstrucoes;
-    public static LinkedList<LinkedList<String>> filaInstrucoes;
+    public static LinkedList<TiposInstrucoes> filaInstrucoes;
+    
+    
+    public static LinkedList<TiposInstrucoes> getFilaInstrucoes() {
+    	return filaInstrucoes;
+    }
+    
     
     Instrucoes(int numeroInstrucoes, int numeroTiposInstrucoes) {
         this.numeroInstrucoes = numeroInstrucoes;
         this.numeroTiposInstrucoes = numeroTiposInstrucoes;
-        filaInstrucoes = new LinkedList<LinkedList<String>>();
+        filaInstrucoes = new LinkedList<TiposInstrucoes>();
+        TiposInstrucoes tipos;
+		tipos = new TiposInstrucoes();
+		filaInstrucoes.add(tipos);
     }
     
-    public static LinkedList<LinkedList<String>> getFilaInstrucoes() {
-    	return filaInstrucoes;
-    }
 
-    public void construirFilaInstrucoes() {
+    /*public void construirFilaInstrucoes() {
+    	TiposInstrucoes tipos;
+    	
         for (int i = 0; i <= numeroTiposInstrucoes; i ++) {
-        	filaInstrucoes.add(new LinkedList<String>());
+        	tipos = new TiposInstrucoes();
+        	filaInstrucoes.add(tipos);
+        	//DEBUG
+        	System.out.println("tamanho da fila de instruções na posicao "+ i + ":" + filaInstrucoes.get(i).getTiposInstrucoes().size());
         }
-    }
+        //DEBUG
+        System.out.println("tamanho da fila de instruções: " + filaInstrucoes.size());
+        
+    }*/
 
     public void addInstrucao(int id, String instrucao) {
-        filaInstrucoes.get(id).add(instrucao);
+    	
+    	if(filaInstrucoes.get(id) == null) {
+    		TiposInstrucoes tipos;
+    		tipos = new TiposInstrucoes();
+    		filaInstrucoes.add(id, tipos);
+    	}
+    	filaInstrucoes.get(id).getTiposInstrucoes().add(instrucao);	
+    	
+    	//DEBUG
+    	System.out.println("Adicionando instrução");
+    	System.out.println("id: " + id);
+    	System.out.println("instrução: " + instrucao);
+    	System.out.println("fila de instrucoes na posicao " + id + " tem o tamanho: " + filaInstrucoes.get(id).getTiposInstrucoes().size());
     }
 
     public void mostrar() {
@@ -35,7 +62,7 @@ class Instrucoes {
 
         System.out.print("Instruções -> ");
         for(int j = 0; j < numeroInstrucoes; j ++) {
-	        for(String i: filaInstrucoes.get(j)) {
+	        for(String i: filaInstrucoes.get(j).getTiposInstrucoes()) {
 	        	System.out.print(i + " ");
 	        }
         }
